45830e9c3cc55e469c9701d2927ea2058caff894
Jony <dev.jonykarki@gmail.com>
2020-06-14 23:24:38 +0000

    get artist names

--- CharlyricsDataset.py
+++ CharlyricsDataset.py
@@ -1,15 +1,20 @@
 from __future__ import print_function
 import os
+import glob
 import torch
 import numpy as np
+from pathlib import Path
+
 from torch.utils.data import Dataset, DataLoader
+from config import config
+
 
 class CharLyricsDataset(Dataset):
     """ Character Lyrics Dataset """
 
     def __init__(self, folder_path, transform=None):
         self.folder_path = folder_path
-        self.artists = list()
+        self.artists = self.get_artist_list()
         self.transform = transform
 
     def __len__(self):
@@ -17,3 +22,14 @@ class CharLyricsDataset(Dataset):
 
     def __getitem__(self, idx):
         return self.artists[idx]
+
+    def get_artist_list(self):
+        files_list = map(
+            lambda filepath: Path(filepath).stem,
+            glob.glob(os.path.join(self.folder_path, "*.txt")),
+        )
+        # remove -, _ and capitalize
+        names_list = map(
+            lambda string_: string_.replace("-", " ").replace("_", " ").title(), files_list
+        )
+        return list(names_list)
--- config.py
+++ config.py
@@ -4,6 +4,15 @@ import os
 _C = edict()
 config = _C
 
+_C.BASEPATH = os.path.abspath(os.pardir)
+
+# Data folders
 _C.DATA = edict()
-_C.DATA.BASE = os.path.join(os.path.abspath(os.pardir), "data")
+_C.DATA.BASE = os.path.join(_C.BASEPATH, "data")
 _C.DATA.MIDI = os.path.join(_C.DATA.BASE, "midi")
+_C.DATA.LYRICS = os.path.join(_C.BASEPATH, "lyrics")
+
+# train parameters
+_C.TRAIN = edict()
+_C.TRAIN.BATCH_SIZE = 32
+_C.TRAIN.EPOCHS = 10
