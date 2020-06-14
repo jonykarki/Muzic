32b53ca239df21f2d8e45b2e01bf35f19f82f907
Jony <dev.jonykarki@gmail.com>
2020-06-14 06:31:39 +0000

    added download

---
+++ download.py
@@ -0,0 +1,36 @@
+from __future__ import print_function
+import os
+from sys import platform
+import zipfile
+import shutil
+import glob
+from config import config
+
+try:
+    from urllib.request import urlretrieve
+except ImportError:
+    from urllib import urlretrieve
+
+def download_data():
+    data_folder = config.DATA.BASE
+    if not os.path.exists(os.path.join(data_folder, "midi")):
+        filename = os.path.join(data_folder, "scale_chords.zip")
+        if not os.path.exists(filename):
+            url = "http://www.feelyoursound.com/data/scale_chords_small.zip"
+            print("Downloading data from {}".format(url))
+            urlretrieve(url, filename)
+        try:
+            print("Extracting {}".format(filename))
+            with zipfile.ZipFile(filename) as zipp:
+                zipp.extractall(data_folder)
+            files = glob.glob(os.path.join(data_folder, "midi") + "/*.mid")
+        finally:
+            os.remove(filename)
+        print('Finished Downloading')
+    else:
+        print("Data already exists in {}".format(data_folder))
+
+    return os.path.join(data_folder, "midi")
+
+if __name__ == "__main__":
+    download_data()
